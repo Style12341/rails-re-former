29544add6c33f5457fd43dda1c9f238423a5e29e
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-04-22 22:29:40 -0300

    Change Form to use form_with

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_22_174009) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_23_012504) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
